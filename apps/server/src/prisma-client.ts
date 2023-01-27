a5653953e80bc731c639ecdd1b137dce89432bce
Muhammad Sarwar <rrsarwar109@gmail.com>
2023-01-27 18:37:39 +0500

    fix

--- apps/server/src/prisma-client.ts
+++ apps/server/src/prisma-client.ts
@@ -1,4 +1,27 @@
 import { PrismaClient } from '@prisma/client';
 export const prisma = new PrismaClient({
+  errorFormat: 'pretty',
   // log: ['error', 'info', 'warn', 'query']
 });
+
+async function excludePasswordMiddleware(params, next) {
+  const result = await next(params);
+  delete result?.user?.password;
+  delete result?.user?.OTP;
+  if (result?.length) result.forEach((r) => {
+    delete r?.user?.password;
+    delete r?.user?.OTP;
+  });
+  if (params?.model === 'User' && !params?.args?.select?.password) {
+    delete result?.password;
+    delete result?.OTP;
+    if (result?.length) result.forEach((r) => {
+      console.log(r);
+      delete r?.password;
+      delete r?.OTP;
+    });
+  }
+  return result;
+}
+
+prisma.$use(excludePasswordMiddleware);
