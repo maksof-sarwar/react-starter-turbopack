9bf42611fd4d35e8081c362ff3c368b8ba1d553a
Muhammad Sarwar <rrsarwar109@gmail.com>
2023-02-05 01:37:54 +0500

    fix

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,8 +5,8 @@ import cors from '@fastify/cors'
 import cookie from '@fastify/cookie'
 import { appRouter } from './app';
 import { createContext } from './conext'
-import Turn from 'node-turn'
 import fastifyStatic from '@fastify/static'
+import sensible from '@fastify/sensible'
 import { join } from 'path';
 const FRONTEND = join(process.cwd(), 'client');
 const server = fastify({
@@ -15,6 +15,7 @@ const server = fastify({
 server.register(fastifyStatic, {
   root: FRONTEND,
 });
+server.register(sensible)
 server.register(health, { healthcheckUrl: '/health', exposeUptime: true });
 server.register(cors, { credentials: true, origin: '*', });
 
@@ -28,30 +29,13 @@ server.register(fastifyTRPCPlugin, {
   trpcOptions: { router: appRouter, createContext, },
 });
 server.setNotFoundHandler((req, rep) => {
-  // API 404
-  // if (req.raw.url && req.raw.url.startsWith("/api")) {
-  //   return rep.status(404).send({
-  //     success: false,
-  //     error: {
-  //       kind: "user_input",
-  //       message: "Not Found",
-  //     },
-  //   });
-  // }
-  console.log(join(FRONTEND, "index.html"))
-  rep.status(200).sendFile("index.html");
+  if (req.url.startsWith("/api")) {
+    rep.notFound(`Route ${req.method}:${req.url} not found`)
+  } else
+    rep.status(200).sendFile("index.html");
 });
 (async () => {
   try {
-    var turnServer = new Turn({
-      authMech: 'long-term',
-      credentials: {
-        username: "password",
-      },
-      listeningPort: 9000,
-      debugLevel: 'ALL'
-    });
-    turnServer.start();
     await server.listen({ port: 3000 });
     console.log('listening on port 3000')
   } catch (err) {
