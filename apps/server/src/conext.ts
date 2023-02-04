0d0a0e98d314f1050502dddf8255d126b835d010
Muhammad Sarwar <rrsarwar109@gmail.com>
2023-02-05 01:46:30 +0500

    Update conext.ts

--- apps/server/src/conext.ts
+++ apps/server/src/conext.ts
@@ -1,9 +1,7 @@
 import { inferAsyncReturnType } from '@trpc/server';
 import { CreateFastifyContextOptions } from '@trpc/server/adapters/fastify';
-import { Request, Response } from 'express';
-
-export const createContext = ({ req, res }: CreateFastifyContextOptions) => {
+export function createContext({ req, res }: CreateFastifyContextOptions) {
   const user = { name: req.headers.username ?? 'anonymous' };
-  return { req, res, user } as { req: Request; res: Response, user: Record<string, any> };
+  return { req, res, user };
 }
 export type Context = inferAsyncReturnType<typeof createContext>;
