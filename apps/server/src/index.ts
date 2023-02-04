0665e6a959ec45c0f2c52df5185d8f6e75082dbe
Muhammad Sarwar <rrsarwar109@gmail.com>
2023-02-05 01:22:43 +0500

    f

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -6,9 +6,14 @@ import cookie from '@fastify/cookie'
 import { appRouter } from './app';
 import { createContext } from './conext'
 import Turn from 'node-turn'
-
+import fastifyStatic from '@fastify/static'
+import { join } from 'path';
+const FRONTEND = join(process.cwd(), 'client');
 const server = fastify({
   maxParamLength: 5000,
+});
+server.register(fastifyStatic, {
+  root: FRONTEND,
 });
 server.register(health, { healthcheckUrl: '/health', exposeUptime: true });
 server.register(cors, { credentials: true, origin: '*', });
@@ -21,6 +26,20 @@ server.register(cookie, {
 server.register(fastifyTRPCPlugin, {
   prefix: '/trpc',
   trpcOptions: { router: appRouter, createContext, },
+});
+server.setNotFoundHandler((req, rep) => {
+  // API 404
+  // if (req.raw.url && req.raw.url.startsWith("/api")) {
+  //   return rep.status(404).send({
+  //     success: false,
+  //     error: {
+  //       kind: "user_input",
+  //       message: "Not Found",
+  //     },
+  //   });
+  // }
+  console.log(join(FRONTEND, "index.html"))
+  rep.status(200).sendFile("index.html");
 });
 (async () => {
   try {
