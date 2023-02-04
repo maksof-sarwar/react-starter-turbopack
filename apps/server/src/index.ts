64c9217761f9129868fbf8bb878516ed4224d0de
Muhammad Sarwar <rrsarwar109@gmail.com>
2023-02-05 01:55:53 +0500

    f

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -9,37 +9,34 @@ import fastifyStatic from '@fastify/static'
 import sensible from '@fastify/sensible'
 import { join } from 'path';
 const FRONTEND = join(process.cwd(), 'client');
-const server = fastify({
+const app = fastify({
   maxParamLength: 5000,
 });
-server.register(fastifyStatic, {
+app.register(fastifyStatic, {
   root: FRONTEND,
 });
-server.register(sensible)
-server.register(health, { healthcheckUrl: '/health', exposeUptime: true });
-server.register(cors, { credentials: true, origin: '*', });
+app.register(sensible)
+app.register(health, { healthcheckUrl: '/health', exposeUptime: true });
+app.register(cors, { credentials: true, origin: '*', });
 
-server.register(cookie, {
+app.register(cookie, {
   secret: "my-secret",
   hook: 'onRequest',
   parseOptions: {}
 })
-server.register(fastifyTRPCPlugin, {
+app.register(fastifyTRPCPlugin, {
   prefix: '/trpc',
   trpcOptions: { router: appRouter, createContext, },
 });
-server.setNotFoundHandler((req, rep) => {
+app.setNotFoundHandler((req, rep) => {
   if (req.url.startsWith("/api")) {
     rep.notFound(`Route ${req.method}:${req.url} not found`)
   } else
     rep.status(200).sendFile("index.html");
 });
-(async () => {
-  try {
-    await server.listen({ port: 3000 });
-    console.log('listening on port 3000')
-  } catch (err) {
-    server.log.error(err);
-    process.exit(1);
-  }
-})();
+// app.listen({ port: 3000, host: '0.0.0.0' })
+export default async (req, res) => {
+
+  await app.ready();
+  app.server.emit('request', req, res);
+}
